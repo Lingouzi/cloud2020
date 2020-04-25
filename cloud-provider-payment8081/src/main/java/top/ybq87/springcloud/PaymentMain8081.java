c01ccacf0c31db1970be2a8ddcf8314675b32afe
ly <664162337@qq.com>
2020-04-25 21:43:49 +0800

    main 方法构建

---
+++ cloud-provider-payment8081/src/main/java/top/ybq87/springcloud/PaymentMain8081.java
@@ -0,0 +1,21 @@
+package top.ybq87.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ly
+ * @blog http://www.ybq87.top
+ * @github https://github.com/Lingouzi
+ * @email 664162337@qq.com
+ * @wechat ly19870316 / 公众号：林子曰
+ * @date 2020/4/25
+ */
+@SpringBootApplication
+public class PaymentMain8081 {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8081.class);
+    }
+    
+}
