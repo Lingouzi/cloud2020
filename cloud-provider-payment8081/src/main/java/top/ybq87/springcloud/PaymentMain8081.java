ebd615c0cf5cce58ed88889229cec7a952f4779a
ly <664162337@qq.com>
2020-04-26 21:46:33 +0800

    服务提供者注册完毕

--- cloud-provider-payment8081/src/main/java/top/ybq87/springcloud/PaymentMain8081.java
+++ cloud-provider-payment8081/src/main/java/top/ybq87/springcloud/PaymentMain8081.java
@@ -2,6 +2,7 @@ package top.ybq87.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ly
@@ -12,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/4/25
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8081 {
     
     public static void main(String[] args) {
