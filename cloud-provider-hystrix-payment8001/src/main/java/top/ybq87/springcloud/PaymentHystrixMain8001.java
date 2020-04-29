8a53a7ca2f3999814cc991327ea79cca69f0a1e8
ly <664162337@qq.com>
2020-04-29 23:17:24 +0800

    服务提供者降级处理

--- cloud-provider-hystrix-payment8001/src/main/java/top/ybq87/springcloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/top/ybq87/springcloud/PaymentHystrixMain8001.java
@@ -2,8 +2,11 @@ package top.ybq87.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 /**
+ * 如果使用了 spring-cloud-starter-netflix-eureka-client 包
+ * 可以不需要写 @EnableEurekaClient 注解，都会自动注册到 eureka 了，所以写不写都没问题
  * @author ly
  * @blog http://www.ybq87.top
  * @github https://github.com/Lingouzi
@@ -12,6 +15,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/4/29
  */
 @SpringBootApplication
+// @EnableEurekaClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     
     public static void main(String[] args) {
--- cloud-provider-hystrix-payment8001/src/main/java/top/ybq87/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/top/ybq87/springcloud/service/PaymentService.java
@@ -1,5 +1,7 @@
 package top.ybq87.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.stereotype.Service;
@@ -23,15 +25,39 @@ public class PaymentService {
         return Thread.currentThread().getName() + " id:" + id + " ok..";
     }
     
+    /**
+     * 测试超时
+     * @param id
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "payment_timeoutHandler", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000")
+    })
     public String payment_timeout(String id) {
-        // 休眠几秒钟
-        try {
-            TimeUnit.SECONDS.sleep(3);
-            System.out.println("......timeout.." + atomicInteger.incrementAndGet());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        int x = atomicInteger.incrementAndGet();
+        if (x % 2 == 0) {
+            // 模拟超时
+            // 休眠几秒钟
+            try {
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println("......timeout.." + x);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // 模拟异常
+            x = 10 / 0;
         }
         return Thread.currentThread().getName() + " id:" + id + " timeout..";
+    }
+    
+    /**
+     * 超时或者异常的 fallback 方法
+     * @param id
+     * @return
+     */
+    public String payment_timeoutHandler(String id) {
+        return Thread.currentThread().getName() + ">>> 系统繁忙 >>>>" + " timeout :)";
     }
     
 }
