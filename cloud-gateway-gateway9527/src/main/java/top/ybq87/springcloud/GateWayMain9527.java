c761ce0eabf4387f714b3d65ea14ee2e49a98164
ly <664162337@qq.com>
2020-05-01 17:48:10 +0800

    基本配置使用

---
+++ cloud-gateway-gateway9527/src/main/java/top/ybq87/springcloud/GateWayMain9527.java
@@ -0,0 +1,22 @@
+package top.ybq87.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author ly
+ * @blog http://www.ybq87.top
+ * @github https://github.com/Lingouzi
+ * @email 664162337@qq.com
+ * @wechat ly19870316 / 公众号：林子曰
+ * @date 2020/5/1
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
